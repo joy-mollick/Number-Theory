64542cf8f87ab449ee021ca2df77f594096f5ac9
Nayeem Mollick Joy <nayeemjoy59@gmail.com>
2019-08-08 16:26:17 -0700

    Add files via upload

---
+++ SPOJ-TIPTOP - Tip Top Game.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+int t ;
+scanf("%d", &t);
+int cs = 1;
+while(t--)
+{
+long long int num;
+scanf("%lld", &num);
+long long int srt = sqrt(num) ;
+if( srt*srt == num )
+printf("Case %d: Yes\n", cs);
+else
+printf("Case %d: No\n", cs);
+cs++;
+}
+}
+
+    Author Sushant
+    Date 00:51
+    Comments 1 comment
+
