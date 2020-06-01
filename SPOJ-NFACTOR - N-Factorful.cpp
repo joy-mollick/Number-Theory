07d6fcc1739f076ccf8996e180d1f78b689d6e5c
Nayeem Mollick Joy <nayeemjoy59@gmail.com>
2020-06-01 05:05:20 -0700

    Update SPOJ-NFACTOR - N-Factorful.cpp

--- SPOJ-NFACTOR - N-Factorful.cpp
+++ SPOJ-NFACTOR - N-Factorful.cpp
@@ -1,55 +1,55 @@
-#include <bits/stdc++.h>
 
-using namespace std;
+///  Sieve implementation and dp 
+/// Time-0.34s
 
-const int mx=1e6+1;
+#include<bits/stdc++.h>
 
-bool prime[mx];
-int prm_fct[mx];
-vector<int>v[mx];
+ using namespace std;
 
-void sieve()
-{
-    for(int i=2;i<mx;i++)
-    {
-        if(prime[i])
-        {
-            prm_fct[i]=1;
-            for(int j=2*i;j<mx;j=j+i)
-            {
-                prm_fct[j]++;
-                prime[j]=false;
-            }
-        }
-    }
-}
+ const int mx=1000001;
 
+ bool status[mx];///
+ int factor[mx];
+ int ans[mx][11];
+ int maxi=0;
 
-int main()
+ void sieve()
+ {
+     int i, j;
+     factor[1]=0;
+     for( i = 2; i <mx; i += 1 ) /// we will not iterate through only sqrt ,because many prime numbers will not be cover by dp,as they will not be iterated
+     {
+		 if( status[i]==0)
+		 {
+		     factor[i]=1;
+	 		 for( j = 2*i; j <mx; j += (i) )
+			 {
+				 status[j]=1;
+				 factor[j]++;
+	 		 }
+		 }
+	 }
+ }
 
-{
-   ios::sync_with_stdio(false);
-   cin.tie(NULL);
-   cout.tie(NULL);
-
-   for(int i=0;i<mx;i++)
-   {
-       prime[i]=true;
-       prm_fct[i]=0;
-   }
-   sieve();
-   for(int i=0;i<mx;i++)
-   {
-       v[prm_fct[i]].push_back(i);
-   }
-   int tc,a,b,n;
-   cin>>tc;
-   while(tc--)
-   {
-       cin>>a>>b>>n;
-       vector<int>ad=v[n];
-       int pos2=upper_bound(ad.begin(),ad.end(),b)-ad.begin();
-       int pos1=lower_bound(ad.begin(),ad.end(),a)-ad.begin();
-       cout<<(pos2-pos1)<<endl;
-   }
-}
+ int main()
+ {
+      sieve();
+      /// it will take O(mx*10)
+      for(int i=1;i<mx;i++)
+      {
+          for(int k=0;k<=10;k++)
+          {
+              ans[i][k]=ans[i-1][k];
+          }
+          int factors=factor[i];/// taking factors
+          ans[i][factors]+=1;
+      }
+      int tc,a,b,n;
+      cin>>tc;
+      while(tc--)
+      {
+          cin>>a>>b>>n;
+          cout<<ans[b][n]-ans[a-1][n]<<endl;
+      }
+      return 0;
+ }
